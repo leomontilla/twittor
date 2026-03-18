3ff8e7ac8c8cb60ec5eee04c64260b1f732cb8e8
Leonardo Montilla G. <contact@leomontilla.dev>
2026-03-18 16:23:34 -0400

    Modificaciones para IOS

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 // Caches
-const CACHE_STATIC = 'static-v1';
+const CACHE_STATIC = 'static-v2';
 const CACHE_INMUTABLE = 'inmutable-v1';
 
 const CACHE_DYNAMIC = 'dynamic-v1';
