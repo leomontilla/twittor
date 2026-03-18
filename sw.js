cc45a4de9879314fbee68ecc4c8173ff49aafaba
Leonardo Montilla G. <contact@leomontilla.dev>
2026-03-18 16:40:18 -0400

    apple-mobile deprecated

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 // Caches
-const CACHE_STATIC = 'static-v4';
+const CACHE_STATIC = 'static-v5';
 const CACHE_INMUTABLE = 'inmutable-v1';
 
 const CACHE_DYNAMIC = 'dynamic-v2';
